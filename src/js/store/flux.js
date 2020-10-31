7b907c64f7b35d6eb093a2d989420b41b0d4141f
Carlos84sh <Cs.huelves@gmail.com>
2020-10-31 09:02:32 +0000

    primer commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,32 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			user: []
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			getUser() {
+				const store = getStore();
+				const urlBase = "https://assets.breatheco.de/";
+
+				if (store.user.length === 0) {
+					const endpoint = `${urlBase}apis/fake/contact/agenda/CSanchez`;
+					const config = {
+						method: "GET",
+						cors: true
+					};
+					fetch(endpoint, config)
+						.then(response => {
+							return response.json();
+						})
+						.then(json => {
+							console.log(json);
+							setStore({ user: json.results });
+						})
+						.catch(error => console.log(error));
+				}
+				//(Arrow) Functions that update the Store
+				// Remember to use the scope: scope.state.store & scope.setState()
+			}
 		}
 	};
 };
